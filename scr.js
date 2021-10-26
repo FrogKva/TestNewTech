943ee4967f6a161dc5abdb0fbe65299a968417f5
Your Name <windowsvistaultimate@gmai;.com>
2021-10-26 12:50:16 +0300

    Inital project

--- scr.js
+++ scr.js
@@ -6,7 +6,7 @@ const
     tech = require('./tech.json')
     var getIP = require('ipware')().get_ip;
     io = require('socket.io')(server)
-    server.listen(2007)
+    server.listen(2009)
 
 app.get('/', (next,context) => {
 
@@ -14,8 +14,11 @@ context.sendFile(__dirname + '/index.html');
   var ipInfo = getIP(next);
     console.log(ipInfo);
     // { clientIp: '127.0.0.1', clientIpRoutable: false }
-    
+
+if(!tech.filter(x => x.clientIp === ipInfo.clientIp)[0])    
+{
     tech.push(ipInfo)
+}
 fs.writeFileSync("./tech.json", JSON.stringify(tech, null, "\t"))
 })
 
